4a5e9b5f9982fb1d42b7846e18552e48d244355e
takashi0115 <b1600627@planet.kanazawa-it.ac.jp>
2019-07-15 02:02:43 +0900

    7.15

--- AccelerometerTest/app/src/main/java/com/example/accelerometertest/MainActivity.java
+++ AccelerometerTest/app/src/main/java/com/example/accelerometertest/MainActivity.java
@@ -10,10 +10,28 @@ import android.os.Bundle;
 import android.widget.TextView;
 import java.util.List;
 
+import static java.nio.file.Files.size;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private SensorManager sensorManager;
-    private TextView textView, textInfo;
+    private TextView textView, textInfo, textaction;
+    float sensorX, sensorY, sensorZ;
+
+    double compACC;  //合成加速度
+    float Atotal;   //合成加速度の合計
+    float Aave;     //合成加速度の平均
+    float Adist;    //合成加速度の分散
+
+    int i;
+    int flag;
+
+    int cnt = 0;
+    int cursor = 0;
+    int DIST_LIMIT = 50;
+    Double distribute[] = new Double[DIST_LIMIT];
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,22 +69,101 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float sensorX, sensorY, sensorZ;
+        // float sensorX, sensorY, sensorZ;
 
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             sensorX = event.values[0];
             sensorY = event.values[1];
             sensorZ = event.values[2];
 
+            compACC = Math.sqrt(sensorX * sensorX + sensorY * sensorY + sensorZ * sensorZ); //合成加速度の計算
+
             String strTmp = "加速度センサー\n"
                     + " X: " + sensorX + "\n"
                     + " Y: " + sensorY + "\n"
-                    + " Z: " + sensorZ;
+                    + " Z: " + sensorZ + "\n"
+                    + "\n"
+                    + "行っている行動:";
             textView.setText(strTmp);
 
             //showInfo(event);
         }
     }
+    
+    public void run() {
+        if (cnt < DIST_LIMIT) { //50個まで配列
+            distribute[cnt] = compACC;
+        } else {
+            if (cursor < DIST_LIMIT) {
+                distribute[cursor] = compACC;
+                cursor++;
+            } else {
+                cursor = 0;
+            }
+        }
+        if(size(distribute) < DIST_LIMIT) {
+            act_recognition();
+            cnt++;
+        } else {
+            Atotal = 0;
+            Adist = 0;
+            for (i = 0; i < DIST_LIMIT; i++) {
+                Atotal += distribute[i];
+            }
+
+            Aave = Atotal / DIST_LIMIT;
+
+            for (i = 0; i < DIST_LIMIT; i++) {
+                Adist += (distribute[i] - Aave) * (distribute[i] - Aave);
+            }
+
+            Adist = Adist / DIST_LIMIT;
+
+            act_recognition();
+            cnt++;
+        }
+    }
+
+    public int size(Double[] array){
+        int count = 0;
+        for(Double prop : array){
+            if(prop != null){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void act_recognition(){
+        //行動判別
+        if(0.6 < Adist && Adist < 14){
+            flag = 1;
+        }else if(14 < Adist && Adist < 25){
+            flag = 2;
+        }else if(25 < Adist){
+            flag = 3;
+        }else {
+            flag = 0;
+        }
+
+        if(flag == 1){
+            String strTmp = "walk\n";
+            textView.setText(strTmp);
+        }else if(flag == 2){
+            String strTmp = "stairs down\n";
+            textView.setText(strTmp);
+        }else if(flag == 3){
+            String strTmp = "run\n";
+            textView.setText(strTmp);
+        }else{
+            String strTmp = "stand\n";
+            textView.setText(strTmp);
+        }
+    }
+
+
+
+
 
     // （お好みで）加速度センサーの各種情報を表示
     /*private void showInfo(SensorEvent event){
